e0f955571eec21ae9267b7bde9b2972c0c9ee44e
DevLETi <devleti0106@gmail.com>
2021-11-23 11:33:59 +0900

    build deploy

---
+++ dist/precache-manifest.22d508e607a7b643f89d0a5e79c5256c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13ab158dcc999a86a706",
+    "url": "/css/app.fb0c6e1c.css"
+  },
+  {
+    "revision": "365f1699811b43ec095770b6056574a1",
+    "url": "/images/icons/icon-192x192.png"
+  },
+  {
+    "revision": "fa88118dc1c4927e31c89e14862edf69",
+    "url": "/images/icons/icon-256x256.png"
+  },
+  {
+    "revision": "db126229310d01d24fe7db3198e38080",
+    "url": "/images/icons/icon-384x384.png"
+  },
+  {
+    "revision": "783e4bbf8f67802fb4cf9221a8644ee3",
+    "url": "/images/icons/icon-512x512.png"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "65f32ac96f09e3c933ec32dba71dd87c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13ab158dcc999a86a706",
+    "url": "/js/app.d6763872.js"
+  },
+  {
+    "revision": "85b76d64d4e8d5d74fd6",
+    "url": "/js/chunk-vendors.67e04185.js"
+  },
+  {
+    "revision": "eb8105f5f245aa595c39593fd8c2eede",
+    "url": "/manifest.json"
+  }
+]);
