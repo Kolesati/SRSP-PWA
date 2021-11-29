5a73478c0bf8045c38f67b74553440a03b6e452b
DevLETi <devleti0106@gmail.com>
2021-11-29 20:34:10 +0900

    v1.0

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,31 +11,30 @@ function readJson(file, callback) {
 }
 
 //usage:
-readJson("assets/json/song_data_app.json", function(text){
-    const data = JSON.parse(text);
-    const config = data["configs"][0];
-    const songs = data["songs"];
-    console.log(data);
-    console.log(config);
-    console.log(songs); 
+readJson("https://raw.githubusercontent.com/Kolesati/SRSP-images/main/level_song_data.json", function(text){
+    localStorage.songs = text;
+    // let songs = JSON.parse(text);
+    // console.log(songs);
 });
 
-let chkbox = document.getElementsByClassName("checkSelect");
-// console.log(chkbox);
+let songs = JSON.parse(localStorage.getItem('songs'));
+// console.log(songs)
 
+let chkbox = document.getElementsByClassName("checkSelect");
 let cnt = 0;
 let selectedLevel = Array();
 let selectedArr = Array();
-let selectedIndex = 0;
+let level = 0;
 let selectedElement = "";
 let selectedSong = "";
 let selectedIllust = "";
+let imgBaseUrl = "https://raw.githubusercontent.com/Kolesati/SRSP-images/main/";
 
 function RecommendSong() {
     cnt = 0;
 	selectedLevel = Array();
 	selectedArr = Array();
-	selectedIndex = 0;
+	level = 0;
 
 	selectedElement = "";
 	selectedSong = "";
@@ -51,9 +50,35 @@ function RecommendSong() {
     if(cnt == 0) {
         return window.alert("레벨을 선택해주세요!");
     }
-    selectedIndex = selectedLevel[Math.floor(Math.random()*cnt)];
-    console.log("선택된 level :" + selectedIndex);
 
-    // TODO : 선택된 레벨들을 이용해 array에 후보 곡 목록 넣고 뽑아서 return하는 알고리즘 만들기
-    return selectedIndex;
+    level = selectedLevel[Math.floor(Math.random()*cnt)];
+    console.log("선택된 level :" + level);
+
+    // TODO : 선택된 레벨들을 이용해 array에 후보 곡 목록 넣고 뽑아서 적용시키기
+    selectedSongs = songs[level];
+    selectedSong = selectedSongs[Math.floor(Math.random()*selectedSongs.length)]
+    console.log(selectedSong)
+    
+    // 자켓, level, 수록일, 곡명 선택
+    var jacketLink = imgBaseUrl + selectedSong['jacket_link'];
+    // level은 이미 정의됨
+    var difficulty = selectedSong['difficulty'];
+    var title = selectedSong['title'];
+    var composer = selectedSong['composer'];
+    var releasedDate = selectedSong['date'];
+
+    console.log(jacketLink)
+    console.log(level);
+    console.log(difficulty)
+    console.log(title)
+    console.log(composer)
+    console.log(releasedDate)
+
+    document.getElementById('illust').src = jacketLink;
+    document.getElementById('level').innerText = level;
+    document.getElementById('difficulty').innerText = difficulty;
+    document.getElementById('title').innerText = title;
+    document.getElementById('composer').innerText = composer;
+    document.getElementById('release-date').innerText = releasedDate;
+    return level;
 }
